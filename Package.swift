0c07edf33f9e71d4210f7f89df9058d7b334d286
H3ARTgg <nomaxismax@gmail.com>
2024-10-31 08:26:42 +0300

    initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsFixed",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "YandexMapsFixed",
+            targets: ["YandexMapsFixed"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "https://github.com/c-villain/YandexMapsMobile/releases/download/4.8.1/YandexMapsMobile.xcframework.zip",
+            checksum: "dc0ba45bfc47f0c3bd3f864a7a047981b512f4def9960a2e421eef90936cb465"
+        ),
+        .target(
+            name: "YandexMapsFixed",
+            dependencies: ["YandexMapsMobile"],
+            linkerSettings: [
+                .unsafeFlags(["-ObjC"])
+            ]
+        )
+    ]
+)
